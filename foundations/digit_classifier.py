c59edbf4ec8ee06a2010c5396beac05ce528ced5
Ashwani Rathee <ashwanirathee.work@gmail.com>
2026-04-24 10:18:14 -0700

    Update Digit classifier (Training 4/4)

--- foundations/digit_classifier.py
+++ foundations/digit_classifier.py
@@ -13,6 +13,14 @@ class Solution(nn.Module):
         self.second_layer = nn.Linear(512,10)
         self.sigmoid_layer = nn.Sigmoid()
 
+        self.model = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(512,10),
+            nn.Sigmoid()
+        )
+
     def forward(self, images: TensorType[float]) -> TensorType[float]:
         torch.manual_seed(0)
         # images shape: (batch_size, 784)
